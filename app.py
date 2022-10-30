f93c03aba0954510b9de15e08f8a9c3877e79c5b
joe841180 <joe841180@gmail.com>
2022-10-31 03:47:40 +0800

    date base

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from linebot.exceptions import (
 )
 from linebot.models import *
 import json
+import psycopg2
 
 app = Flask(__name__)
 
@@ -18,7 +19,8 @@ line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
 
 # 讀、存檔
-class handlingFile:
+class handlingData:
+
     salaryData = "salaryData.json"
     sample = {
         "workDays":[
@@ -36,25 +38,62 @@ class handlingFile:
             "laborInsurance":False, #勞保
         }
     }
-    def read(self):
-        with open(self.salaryData,"r") as f:
-            data = json.loads(f.read())
-            return data
+    def __init__(self):
+        conn = psycopg2.connect(database="資料庫名稱",
+            user="uqelofwpevaxsf",
+            password="76d0dc1ff97412ed81c65785829d463e2b2f9cd453911647deab9a509d6ad87f",
+            host="ec2-23-20-140-229.compute-1.amazonaws.com",
+            port="5432")
+        self.conn = conn
+        self.cursor = conn.cursor()
+        print("Opened database successfully")
 
     def write(self, data):
         with open(self.salaryData,"w") as f:
             data = json.dumps(data)
             f.write(data)
 
-# def inputDate(message):
-#     data = handlingFile.read() #讀取
-    
-#     if message.find(""):
-#         data[""] = ""
-    
-#     handlingFile.write(data)#寫入
+    # 建立資料表
+    def creatDB(self):
+        cursor = self.cursor()
+        cursor.execute("CREATE TABLE userdata (id serial PRIMARY KEY, name VARCHAR(50), userid VARCHAR(50));")
+        print("Create table successfully")
+        cursor.close()
 
+    # 寫入資料
+    def write(self):
+        cursor = self.cursor()
+        cursor.execute("INSERT INTO userdata (name, userid) VALUES (%s, %s);", ("小明", "a123456"))
+        cursor.execute("INSERT INTO userdata (name, userid) VALUES (%s, %s);", ("小王", "b654321"))
+        cursor.execute("INSERT INTO userdata (name, userid) VALUES (%s, %s);", ("小華", "c987654"))
+        print("Inserted 3 rows of data")
+        self.conn.commit()
+        cursor.close()
 
+    # 讀取資料
+    def read(self):
+        cursor = self.cursor()
+        cursor.execute("SELECT * FROM userdata;")#選擇資料表userdata
+        rows = cursor.fetchall() #讀出所有資料
+
+        for row in rows:   #將讀到的資料全部print出來
+            print("Data row = (%s, %s, %s)" %(str(row[0]), str(row[1]), str(row[2])))
+
+    # 更新資料
+    def update(self):
+        cursor = self.cursor()
+        cursor.execute("UPDATE inventory SET userid = %s WHERE name = %s;", ("d123789", "小明"))
+        self.conn.commit()
+        print("Updated 1 row of data")
+        cursor.close()
+
+    # 刪除資料
+    def delete(self):
+        cursor = self.cursor()
+        cursor.execute("DELETE FROM userdata WHERE name = %s;", ("小華",))
+        print("Deleted 1 row of data")
+        self.conn.commit()
+        cursor.close()
 
 
 
@@ -78,12 +117,13 @@ def callback():
 def handle_message(event):
     # message = TextSendMessage(text=event.message.text)
     message = event.message.text
+    db = handlingData()
 
     # 儲存日期、計算時數
     if message == "123":
-        res = message
-    else:
         res = "error"
+    else:
+        res = message
 
     # res = "test"
 
